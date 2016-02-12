98a1ff92b95aa67422e89d7ee5128a5442ee57a6
Varshit <varshit97@gmail.com>
2016-02-12 21:20:48 +0530

    Made the client interactive with ls command

--- client.py
+++ client.py
@@ -14,29 +14,35 @@ host = socket.gethostname()     # Get local machine name
 port = 60001                    # Reserve a port for your service.
 
 s.connect((x, port))
-s.send("ls")
-lsoutput = s.recv(1024) 
-checkvalue = s.recv(1024)
-filename = checkvalue.split('  ')[1]+'1'
-checkvalue = checkvalue.split('  ')[0]
-# print filename
-# print checkvalue
-with open(filename, 'wb') as f:
-    print 'file opened'
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
 
-f.close()
-(status,output)=commands.getstatusoutput('md5sum %s' %(filename))
-if(checkvalue==output.split('  ')[0]):
-	print 'File Transfered properly'
+while True:
+    command=raw_input("Enter Command : ")
+    if command=='ls':
+        s.send("ls")
+        lsoutput = s.recv(1024) 
+        print lsoutput
+    if command=='exit':
+        break
+    else:
+        filename='mytext.txt'
+        #checkvalue = s.recv(1024)
+        #filename = checkvalue.split('  ')[1]+'1'
+        #checkvalue = checkvalue.split('  ')[0]
+        with open(filename, 'wb') as f:
+            print 'file opened'
+            while True:
+                print('receiving data...')
+                data = s.recv(1024)
+                print('data=%s', (data))
+                if not data:
+                    break
+                # write data to a file
+                f.write(data)
+        f.close()
+#        (status,output)=commands.getstatusoutput('md5sum %s' %(filename))
+#        if(checkvalue==output.split('  ')[0]):
+#	    print 'File Transfered properly'
 # print output.split('  ')[0]
-print('Successfully get the file')
+#print('Successfully get the file')
 s.close()
 print('connection closed')
--- server.py
+++ server.py
@@ -4,6 +4,15 @@ from subprocess import Popen,PIPE
 import socket                   # Import socket module
 import commands
 
+
+def calculateMD5Sum(fileName):
+    (status,output)=commands.getstatusoutput('md5sum %s'%(fileName))
+    return str(output.split('  '))[0]
+
+def showFiles():
+    res=Popen(['ls'],stdout=PIPE)
+    return res.stdout.read()
+
 port = 60001                    # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
@@ -19,18 +28,17 @@ while True:
     data = conn.recv(2048)
     print('Server received', repr(data))
     if data=='ls':
-        res=Popen(['ls'],stdout=PIPE)
-        conn.send(res.stdout.read())
-    filename='mytext.txt'
-    (status,output)=commands.getstatusoutput('md5sum mytext.txt')
-    conn.send(str(output))
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
+        fileList=showFiles()
+        conn.send(fileList)
+    elif data.split(' ')[0]=='Download':
+        filename=data.split(' ')[1]
+        f = open(filename,'rb')
+        l = f.read(1024)
+        while(l):
+            conn.send(l)
+            print('Sent ',repr(l))
+            l = f.read(1024)
+        f.close()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print('Done sending')
     # conn.send('Thank you for connecting')
